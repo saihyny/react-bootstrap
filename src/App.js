4f517711d743038d76fb91698f8fced24e6c8ac4
saihyny <satyasaikiranrocks@gmail.com>
2023-12-06 14:42:32 +0530

    Added Login

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider, Link} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
+
 import "./App.css";
 import "./navbar.css";
 import HomePage from "./HomePage";
 import CartContextProvider from "./Contexts/CartContextProvider";
 import Store from "./Store";
+import AuthForm from "./Login/AuthForm";
 const Router = createBrowserRouter([
   { path: "/About", element: <AboutPage></AboutPage> },
   {
-    path: "/", element:<HomePage></HomePage>
+    path: "/",
+    element: <HomePage></HomePage>,
   },
   {
-    path: "/Store", element:<Store/>
+    path: "/Store",
+    element: <Store />,
+  },
+  {
+    path: "/Login",
+    element: <AuthForm />,
   }
 ]);
 function App() {
- 
-
   return (
-    
-      <CartContextProvider>
-        <RouterProvider router={Router}>
-       <HomePage>
+    <CartContextProvider>
+      <RouterProvider router={Router}>
+        <HomePage>
           <Link href="/"></Link>
           <Link href="/Store"></Link>
-          <Link href="/About"></Link>
-       </HomePage>
-        </RouterProvider>
-        
-      </CartContextProvider>
-   
+          <Link href="/Aboutss"></Link>
+          <Link href="/Login"></Link>
+        </HomePage>
+      </RouterProvider>
+    </CartContextProvider>
   );
 }
 
---
+++ src/Login/AuthForm.js
@@ -0,0 +1,98 @@
+import { useState, useRef, useContext } from "react";
+import {Navigate} from 'react-router-dom'
+import classes from "./AuthForm.module.css";
+
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [loggedin , setLoggedin] = useState(false);
+ 
+  const email = useRef();
+  const password = useRef();
+ 
+  if(loggedin){
+    
+   return <Navigate to='/Store'/>
+   
+  }
+ 
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const Email = email.current.value;
+    const Password = password.current.value;
+    let URL;
+    if (isLogin) {
+      URL =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAFi-B6uTTvM5DeigNJG7f4pDAADnDxI2w";
+    } else {
+      URL =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAFi-B6uTTvM5DeigNJG7f4pDAADnDxI2w";
+    }
+
+    fetch(URL, {
+      method: "POST",
+      body: JSON.stringify({
+        email: Email,
+        password: Password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "authentication faild";
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+       
+        setLoggedin(true)
+        // window.location.replace('/profile')
+       
+       
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+ 
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input ref={email} type="email" id="email" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input ref={password} type="password" id="password" required />
+        </div>
+        <div className={classes.actions}>
+          <button>Submit</button>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
